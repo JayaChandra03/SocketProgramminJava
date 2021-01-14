16b4ef7aac954beccdf77278dd699d909a53d606
Kowsalya <p.r.kowsalya149@gmail.com>
2021-01-14 13:54:47 +0530

    socket basics

--- src/main/java/com/jay/basics/SocketSample.java
+++ src/main/java/com/jay/basics/SocketSample.java
@@ -1,2 +1,28 @@
+package com.jay.basics;
 
+import java.io.IOException;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.Scanner;
 
+public class SocketSample {
+	public static void main(String[] args) {
+		try (Socket s = new Socket("time.nist.gov",13)){
+			
+			s.setSoTimeout(15000);//15secs
+			try(Scanner scanner  = new Scanner(s.getInputStream())){
+			    while(scanner.hasNext()) {
+			     System.out.println(scanner.nextLine());	
+			   }
+			}catch(Exception e) {
+				throw e;
+			}
+						
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+		}
+	}
+}
